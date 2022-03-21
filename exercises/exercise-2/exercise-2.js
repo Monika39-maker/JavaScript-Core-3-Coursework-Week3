12ecf6966f8dfcae43204983738f2075b31a6e49
Monika39-maker <60827831+Monika39-maker@users.noreply.github.com>
2022-03-21 14:27:37 +0000

    edited exercise2.js file

--- exercises/exercise-2/exercise-2.js
+++ exercises/exercise-2/exercise-2.js
@@ -70,3 +70,66 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
+//Task 1: In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names of the people who belong to the Gryffindor house.
+// - Use array destructuring to extract the values you need out of the array.
+
+function giveNames(arr) {
+  for (let person of arr) {
+    let {firstName, lastName, house, occupation} = person
+    if (house === "Gryffindor" && occupation === 'Student') {
+      console.log(`${firstName} ${lastName}`)
+    }
+    
+  }
+}
+console.log(giveNames(hogwarts))
+
+// Task 2: In `exercise-2.js` write a program that will take the `hogwarts` array as input and display the names of teachers who have pets.
+// - Use array destructuring to extract the values you need out of the array.
+
+function giveTeachersNamesWhoHavePets(arr) {
+  for (let person of arr) {
+    let {pet, occupation, lastName, firstName} = person
+    if (pet !== null && occupation === 'Teacher') {
+      console.log(`${firstName} ${lastName}`)
+    }
+    
+  }
+}
+console.log(giveTeachersNamesWhoHavePets(hogwarts))
+  {
+    firstName: "Severus",
+    lastName: "Snape",
+    house: "Slytherin",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Filius",
+    lastName: "Flitwick",
+    house: "Ravenclaw",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Pomona",
+    lastName: "Sprout",
+    house: "Hufflepuff",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Minerva",
+    lastName: "McGonagall",
+    house: "Gryffindor",
+    pet: null,
+    occupation: "Teacher",
+  },
+  {
+    firstName: "Albus",
+    lastName: "Dumbledore",
+    house: "Gryffindor",
+    pet: "Phoenix",
+    occupation: "Teacher",
+  },
+];
